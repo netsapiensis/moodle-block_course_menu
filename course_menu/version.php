6ba39f932db39078e5bcc91dfff66fe52beb3cb4
netsapiensis <radu.groza@bitstone.eu>
2012-08-09 09:10:24 +0300

    Readme and install notes

--- course_menu/version.php
+++ course_menu/version.php
@@ -40,7 +40,7 @@ if ($blk !== false && is_object($blk)) {
 
 $plugin->version = 2012021501;
 
-$plugin->component  = 'block_course_menu';
-$plugin->release    = '2.0.1';
-$plugin->requires   = 2010112400; // Moodle 2.0
-$plugin->maturity   = MATURITY_STABLE;
+$plugin->component = 'block_course_menu';
+$plugin->release = '2.0.1';
+$plugin->requires = 2010112400; // Moodle 2.0
+$plugin->maturity = MATURITY_STABLE;
