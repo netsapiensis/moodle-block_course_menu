0ee52a3497075ba165c0d2fcbc9751a72ff60f78
netsapiensis <radu.groza@bitstone.eu>
2012-08-27 09:37:25 +0300

    link_with_navigation and version fix

--- course_menu/version.php
+++ course_menu/version.php
@@ -21,9 +21,9 @@
  * ---------------------------------------------------------------------------------------------------------------------
  */
 
-$plugin->version = 2012072401;
+$plugin->version = 2012082701;
 
 $plugin->component  = 'block_course_menu';
 $plugin->release    = '2.3.0';
-$plugin->requires   = 2012061700; // Moodle 2.3
+$plugin->requires   = 2012062500; // Moodle 2.3
 $plugin->maturity   = MATURITY_STABLE;
