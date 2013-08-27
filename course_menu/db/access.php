0903d3157792a1fd1eb081116531c398aabd99d9
radugroza <radu.groza@bitstone.eu>
2013-08-27 16:21:50 +0300

    added block/course_menu:myaddinstance capability

--- course_menu/db/access.php
+++ course_menu/db/access.php
@@ -25,16 +25,22 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
+    'block/course_menu:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
     'block/course_menu:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
-
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
-
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
 );
